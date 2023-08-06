99abe3b0a798a85c68a7be3d232fff71cc5e7692
vonlooten <kqkqkqkqkqkqkq@pm.me>
2023-08-06 16:54:37 +0200

    chore : cleanup

--- src/components/BlockList/index.tsx
+++ src/components/BlockList/index.tsx
@@ -8,25 +8,22 @@ interface Props {
   blockListData: BlockListQuery;
 }
 
-
 function BlockList(props:BlockListQuery) {
   const { loading, error, data } = useQuery(QUERY_BLOCK_LIST);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
 
-
   return (
-
     data.blocks.edges.map(({ node }: { node: Block }) => { 
       console.log(node);
       return (
-        <Box key={node.id}>
-          {/* Render your content here using the node data */}
+        <Box key={node.id} sx={{marginY: "1.0em"}}>
+          {/* Render content here using the node data */}
           <Typography variant={"body1"} fontWeight={600}>{node.height}</Typography>
+          <Typography variant={"body2"}>Number of transactions: {node.transactionCount}</Typography>
         </Box>
       );
     })
-   
   )
 }
 
